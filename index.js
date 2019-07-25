1f576babc09007506dd5a61d6e5419f95649d46b
zhouxin <zxh87516@qq.com>
2019-07-25 14:15:46 +0800

    1

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 import React, {Component} from 'react'
 import {
     StyleSheet,
@@ -6,8 +5,7 @@ import {
     Text,
     Image,
 } from 'react-native'
-
-// import {PreDimens, PreColors, PreStyles, PreActionSheet, PreTouchLayout} from 'pre_box'
+import PropTypes from 'prop-types';
 import PreDimens from './res/PreDimens';
 import PreColors from './res/PreColors';
 import PreStyles from './res/PreStyles';
@@ -16,28 +14,37 @@ import PreTouchLayout from './res/PreTouchLayout';
 import icon_wechat from './res/images/icon_wechat.png'
 import icon_circle from './res/images/icon_circle.png'
 
+const iconAllList = {"好友": icon_wechat, "朋友圈": icon_circle, "微博": icon_circle, "QQ": icon_circle};
 
-const TOP_LIST = [
-    {id: 1, name: '好友', icon: icon_wechat},
-    {id: 2, name: '朋友圈', icon: icon_circle},
-]
-
-const TOP_LIST_SINGLE = [
-    {id: 1, name: '好友', icon: icon_wechat}
-]
 
 export default class SharePanelComponent extends Component {
 
-    static defaultProps = {}
+
+    static propTypes = {
+        iconList: PropTypes.arrayOf(PropTypes.string),
+        shareUrl: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        desc: PropTypes.string.isRequired,
+        imgUrl: PropTypes.string.isRequired,
+    };
+
+
+    static defaultProps = {
+        iconList: ["微信", "朋友圈"],
+    }
 
     constructor(props) {
         super(props)
+        let {desc} = props;
+        //检查类型
+        this._typeCheck();
         // 初始状态
         this.state = {}
     }
 
     componentWillMount() {
     }
+
 
     render() {
         return (
@@ -62,20 +69,20 @@ export default class SharePanelComponent extends Component {
      * 头部
      */
     renderTopList = () => {
-        const {single} = this.props
-        let topList = single ? TOP_LIST_SINGLE : TOP_LIST
+        const {iconList} = this.props;
+
         let viewArr = []
-        for (let i = 0; i < topList.length; i++) {
-            let model = topList[i]
+        iconList.map((item,index)=>{
             viewArr.push(
-                this.renderItemContent(model)
+                this.renderItemContent(item, index)
             )
-        }
+        })
         return (
-            <View style={[styles.viewList, {justifyContent: single ? 'center' : 'space-between'}]}>
+            <View style={[styles.viewList, {justifyContent: iconList.length === 1 ? 'center' : 'space-between'}]}>
                 {viewArr}
             </View>
         )
+
     }
 
     /**
@@ -83,29 +90,57 @@ export default class SharePanelComponent extends Component {
      * @param model
      * @returns {XML}
      */
-    renderItemContent = (model) => {
-        if (!model) {
+    renderItemContent = (name, i) => {
+        if (!name) {
             return false
         }
         return (
             <PreTouchLayout
+                key={i}
                 underlayColor={PreColors.white}
                 onPress={() => {
-                    if (this.props.actionButton) {
-                        this.props.actionButton(model)
-                    }
+                    console.log("调用原生分享")
                 }}
+
             >
                 <View style={{alignItems: 'center'}}>
-                    <Image source={model.icon} style={styles.ivShare} resizeMode={'contain'}/>
+                    <Image source={iconAllList[name]} style={styles.ivShare} resizeMode={'contain'}/>
                     <View style={[PreStyles.center]}>
                         <Text style={styles.tvList}>{
-                            model.name
+                            name
                         }</Text>
                     </View>
                 </View>
             </PreTouchLayout>
         )
+    }
+
+    /**
+     * 父组件上的类型检查
+     * @private
+     */
+    _typeCheck = () => {
+        let {desc, imgUrl, title, shareUrl, iconList} = this.props;
+        iconList.map(item => {
+
+            let isHasProperty = iconAllList[item];
+            if (!isHasProperty) {
+                throw  new Error(`分享方式不支持${item}`)
+            }
+        })
+        //
+        if (desc === undefined) {
+            throw  new Error("分享组件desc必填属性缺失")
+        }
+        if (imgUrl === undefined) {
+            throw  new Error("分享组件imgUrl必填属性缺失")
+        }
+        if (title === undefined) {
+            throw  new Error("分享组件title必填属性缺失")
+        }
+        if (shareUrl === undefined) {
+            throw  new Error("分享组件shareUrl必填属性缺失")
+        }
     }
 
     /**
@@ -119,15 +154,15 @@ export default class SharePanelComponent extends Component {
     }
 
     open() {
-        // const ref = this.props.modalRef
         this.refs.shareModal.open()
     }
 
     close() {
-        // const ref = this.props.modalRef
         this.refs.shareModal.close()
         this.props.onClose && this.props.onClose('1')
     }
+
+
 }
 
 const styles = StyleSheet.create({
